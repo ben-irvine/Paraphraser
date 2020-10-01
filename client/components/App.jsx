3bbf5f59b55ff8301d6077cc10c2ec24ad619453
Ben Irvine <benji72027@gmail.com>
2020-10-01 15:36:28 +1300

    simple form set up

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 const App = () => {
   return (
-    <h1>React development has begun!</h1>
+    <>
+      <h1>Paraphraser</h1>
+      <h3>They're your words now!</h3>
+
+      <h5>Enter the text you would like to paraphrase below:</h5>
+      <form>
+        <label htmlFor="text">
+          <input size="100" width="1000px" type="text" />
+        </label>
+        <label htmlFor="submit">
+          <input type="submit" value="Submit"></input>
+        </label>
+      </form>
+    </>
   )
 }
 
